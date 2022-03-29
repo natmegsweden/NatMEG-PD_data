b54cc04956a159044aa0802bf0bd214906dd58a6
mcvinding <mikkel.vinding@gmail.com>
2022-03-29 07:15:29 +0200

    anonymization and notes in bidsify

--- scripts/bidsify/temp/BIDS_MEG.m
+++ scripts/bidsify/temp/BIDS_MEG.m
@@ -1,6 +1,5 @@
-%% 
 %% MEG-BIDS, the brain imaging data structure extended to magnetoencephalography.
-% 
+% MCV: CHANGE HEADER INFO IN FINAL VERSION
 % 
 % In this script, you will use the FieldTrip toolbox to reorganize MEG data 
 % to BIDS.
@@ -13,17 +12,22 @@
 % you run this script, it is also a good idea to restore the PATH. Next, use the 
 % MATLAB function |addpath( ... )| to add the path where you downloaded FieldTrip.
 
-restoredefaultpath
-addpath('/home/igocom/fieldtrip/') % Add the path   
-ft_defaults
-
-meg_path = '/home/igocom/PD_data_test';
-%% 
 % If you have several variables with the same names, it might also be good close 
 % all open windows and clear all figures and variables from your workspace.
 
 close all       % Close all open windows
 clear all       % Clear all variables from the workspace
+restoredefaultpath
+% addpath('/home/igocom/fieldtrip/') % Add the path
+addpath('~/fieldtrip/fieldtrip') % Add the path
+ft_defaults
+
+raw_path        = '/archive/20080_PD_EBRAINS/ORIGINAL/MEG';
+meg_path        = '/home/mikkel/PD_long/data_share/temp';
+% meg_path       = '/home/igocom/PD_data_test';
+subj_data_path  = '/home/mikkel/PD_long/subj_data/';
+bidsroot        = '/home/mikkel/PD_long/data_share/temp'; 
+
 %% 
 % 
 % 
@@ -33,67 +37,98 @@ clear all       % Clear all variables from the workspace
 
 %% Define subjects and sessions
 
-subjects_and_dates = {
-                'NatMEG_0521/190417/'
-                'NatMEG_0522/190426/'
-                'NatMEG_0523/190429/'
-                'NatMEG_0524/190429/'
-
-             };
-
-subjects =  {
-                'NatMEG_0521'
-                'NatMEG_0521'
-                'NatMEG_0521'
-                'NatMEG_0521'
-            };
-                
-filenames = {
-        'rest_ec_mc_avgtrans_tsss_corr95.fif'
-        'rest_ec_mc_avgtrans_tsss_corr95.fif'
-        'rest_ec_mc_avgtrans_tsss_corr95.fif'
-        'rest_ec_mc_avgtrans_tsss_corr95.fif'
+% subjects_and_dates = {
+%                 'NatMEG_0521/190417/'
+%                 'NatMEG_0522/190426/'
+%                 'NatMEG_0523/190429/'
+%                 'NatMEG_0524/190429/'
+% 
+%              };
+% 
+% subjects =  {
+%                 'NatMEG_0521'
+%                 'NatMEG_0521'
+%                 'NatMEG_0521'
+%                 'NatMEG_0521'
+%             };
+%                 
+% filenames = {
+%         'rest_ec_mc_avgtrans_tsss_corr95.fif'
+%         'rest_ec_mc_avgtrans_tsss_corr95.fif'
+%         'rest_ec_mc_avgtrans_tsss_corr95.fif'
+%         'rest_ec_mc_avgtrans_tsss_corr95.fif'
+%         
+%             };
         
-            };
+%% Define subjects and sessions - NEW
+% All subject relevant data is now stored in the 'alldata.mat' file (see
+% other script how it is generated). Import and use for the bidsify loop.
+     
+load(fullfile(subj_data_path, 'alldata'));
+       
+subjects_and_dates = alldata.subject_date;
+
 %% 
 % *Run loop*
+n_sessions = 1;
 
+%% Genral info 
+% Common for all participants or info written to dataset_description file.
+general = [];
+general.method = 'copy'; % the original data is in a BIDS-compliant format and can simply be copied
+general.bidsroot = bidsroot;
+
+general.InstitutionName                 = 'Karolinska Institute';
+general.InstitutionalDepartmentName     = 'Department of Clinical Neuroscience';
+general.InstitutionAddress              = 'Nobels väg 9, 171 77, Stockholm, Sweden';
+general.dataset_description.Name        = 'NatMEG_PD_database';
+general.dataset_description.BIDSVersion = 'v1.5.0';
+
+cfg.coordsystem.MEGCoordinateSystem     = 'Neuromag';
+
+
+
+% MCV: MISSING FIELDS WHERE WE NEED TO FIND THE CORRECT INFO
+% general.dataset_description.EthicsApprovals =    % ???
+% cfg.meg.DewarPosition                 = 'upright'; % REQUIRED. Position of the dewar during the MEG scan: "upright", "supine" or "degrees" of angle from vertical: for example on CTF systems, upright=15??, supine = 90??.
+% cfg.meg.SoftwareFilters               = 'n/a'; % REQUIRED. List of temporal and/or spatial software filters applied, orideally key:valuepairsofpre-appliedsoftwarefiltersandtheir parameter values: e.g., {"SSS": {"frame": "head", "badlimit": 7}}, {"SpatialCompensation": {"GradientOrder": Order of the gradient compensation}}. Write "n/a" if no software filters applied.
+% cfg.meg.DigitizedLandmarks            = 'true'; % REQUIRED. Boolean ("true" or "false") value indicating whether anatomical landmark points (i.e. fiducials) are contained within this recording.
+% cfg.meg.DigitizedHeadPoints           = 'true'; % REQUIRED. Boolean ("true" or "false") value indicating whether head points outlining the scalp/face surface are contained within this recording.
+% cfg.events = ???  %MCV: I am not sure how to add this info. The values
+% are 1 = start, 64 = stop. Though not all have 
+% MCV: OTHER FIELDS/ISSUES WE NEED TO FIGURE OUT
+%cfg.meg.MaxMovement =           % Maxfilter is applied? FT does not read this from file?
+%cfg.meg.AssociatedEmptyRoom =  % Empty room files??
 
 %% Run loop
-
-for subindx=1:numel(subjects)
-  for runindx=1:2
-    d = dir(sprintf('/home/igocom/PD_data_test/NatMEG_0521/190417/rest_ec_mc_avgtrans_tsss_corr95.fif', subjects{subindx}, runindx));
+for subindx=1:numel(subjects_and_dates)
+  for runindx=1:n_sessions
+%     d = dir(sprintf('/home/igocom/PD_data_test/NatMEG_0521/190417/rest_ec_mc_avgtrans_tsss_corr95.fif', subjects{subindx}, runindx));
+    d = find_files(fullfile(raw_path, subjects_and_dates{subindx}), 'rest_ec_mc_avgtrans_tsss_corr95');
     if isempty(d)
       % for most subjects the data was recorded in a single run
       % in that case run 2 does not exist
       continue
     else
-      origname = fullfile(d.folder, d.name);
-      anonname = fullfile(d.folder, sprintf('%s_%d.fif', subjects{subindx}, runindx));
-      disp(anonname); % this is just an intermediate name, the final name will be assigned by data2bids
+      origname = fullfile(raw_path, subjects_and_dates{subindx}, d{1});
+%       anonname = fullfile(raw_path, sprintf('%s_%d.fif', alldata.anonym_id{subindx}, runindx));
+%       disp(anonname); % this is just an intermediate name, the final name will be assigned by data2bids
     end
     
-   
-
-    cfg = [];
-    
-    cfg.bidsroot = '/home/igocom/bids';  % write to the present working directory
-    cfg.sub = subjects{subindx};
-    cfg.run = runindx;
-    cfg.dataset = anonname; % this is the intermediate name
-    
+    cfg = general;
+%     cfg.bidsroot = '/home/igocom/bids';  % write to the present working directory
+    cfg.sub      = alldata.anonym_id{subindx};
+    cfg.task     = 'rest';
+    cfg.TaskName = 'rest';
+%     cfg.run      = runindx;
+    cfg.dataset  = origname; % this is the intermediate name
     cfg.datatype = 'meg';
-    cfg.method = 'copy'; % the original data is in a BIDS-compliant format and can simply be copied
     
-    cfg.InstitutionName             = 'Karolinska Institute';
-    cfg.InstitutionalDepartmentName = 'Department of Clinical Neuroscience';
-    cfg.InstitutionAddress          = 'Tomtebodavägen 18A, 171 77, Stockholm, Sweden';
-    
-    % required for dataset_description.json
-    cfg.dataset_description.Name                = 'PD_data_share';
-    cfg.dataset_description.BIDSVersion         = 'v1.5.0';
-
+    cfg.participants.age = alldata.agebin(subindx);
+%     cfg.participants.handedness = handedness(i);   % THIS INFO IS MISSING
+    cfg.participants.clinical_state = alldata.group(subindx);
+    cfg.participants.sex = alldata.sex(subindx);
+  
     try
       data2bids(cfg);
     catch
---
+++ scripts/bidsify/temp/anonymize_subjdata.m
@@ -0,0 +1,108 @@
+%% Import subject data from database and anonymize. 
+%
+% <Ref>
+%
+% Import databases with subject id and clinical data. Generate a key for
+% pseudonymization. Save as database and csv.
+
+subj_data_path  = '/home/mikkel/PD_long/subj_data/';
+
+% Read subjects
+subj_file = fullfile(subj_data_path, 'subjects_and_dates.csv');
+
+subj_dat = readtable(subj_file);
+subj_names = table2cell(subj_dat(:,1));
+tmp = subj_dat(:,4);
+has_mri = table2array(tmp)==1;
+recdate = (table2cell(subj_dat(:,3)));
+
+% Remove old subjects
+idxer = subj_dat{:,1} > 500;
+has_mri  = has_mri(idxer);
+subjects = subj_names(idxer);
+recdate  = recdate(idxer);
+
+% Arrange subject id
+for ss = 1:length(subjects)
+    subjects{ss} = ['0', num2str(subjects{ss})];
+end
+
+% Arrange subject and date folders
+subject_date = cell(size(subjects));
+for ss = 1:length(recdate)
+    subject_date{ss} = fullfile(['NatMEG_',subjects{ss}], num2str(recdate{ss}));
+end
+
+%% Read other data
+subj_file2 = fullfile(subj_data_path, 'subj_data_anonymised.csv');
+subj_dat2 = readtable(subj_file2);
+
+subj_names2 = table2cell(subj_dat2(:,6));
+for ss = 1:length(subj_names2)
+    subj_names2{ss} = ['0', num2str(subj_names2{ss})];
+end
+
+%% Generate random name order
+rand_subj = randperm(numel(subjects));
+anonym_id = cell(size(subjects));
+for ss = 1:length(rand_subj)
+    anonym_id{ss}     = num2str(rand_subj(ss) , '%03d');
+end
+
+%% Bin age data
+age = table2array(subj_dat2(:,4));
+agebin = cell(size(age));
+
+for aa = 1:length(age)
+    ax = age(aa);
+    if 41 <= ax && ax <= 45
+        agebin{aa} = '41-45';
+    elseif 46 <= ax && ax <= 50
+        agebin{aa} = '46-50';
+    elseif 51 <= ax && ax <= 55
+        agebin{aa} = '51-55';
+    elseif 56 <= ax && ax <= 60
+        agebin{aa} = '45-60';       
+    elseif 61 <= ax && ax <= 65
+        agebin{aa} = '61-65';
+    elseif 66 <= ax && ax <= 70
+       agebin{aa} = '66-70';
+    elseif 71 <= ax && ax <= 75
+       agebin{aa} = '70-75';
+    elseif 76 <= ax && ax <= 80
+       agebin{aa} = '76-80';      
+    end
+end
+
+%% Fix variables
+group = table2array(subj_dat2(:,1));
+for gg = 1:numel(group)
+    if strcmp(group{gg}, 'Control ')
+        group{gg} = 'Control';
+    elseif strcmp(group{gg}, 'Patient ')
+        group{gg} = 'Patient';
+    end
+end
+
+sex = table2array(subj_dat2(:,5));
+for gg = 1:numel(group)
+    if strcmp(sex{gg}, 'F ')
+        sex{gg} = 'F';
+    elseif strcmp(sex{gg}, 'M ')
+        sex{gg} = 'M';
+    end
+end
+
+MoCA = table2array(subj_dat2(:,7));
+FAB = table2array(subj_dat2(:,8));
+BDI = table2array(subj_dat2(:,9));
+
+% MISSING UPDRS DATA
+
+%% Combine as table
+alldata = table(subjects, subject_date, anonym_id, sex, agebin, group, MoCA, FAB, BDI);
+
+%% Save
+save(fullfile(subj_data_path, 'alldata'), 'alldata'); disp('done')
+
+%END
