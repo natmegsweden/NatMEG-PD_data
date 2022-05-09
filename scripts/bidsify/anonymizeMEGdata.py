ecb480e71b1393cd05d9d8a792b5f3da8fa0663f
mcvinding <mikkel.vinding@gmail.com>
2022-05-09 21:13:01 +0200

    edit MEG data

--- scripts/bidsify/anonymizeMEGdata.py
+++ scripts/bidsify/anonymizeMEGdata.py
@@ -14,8 +14,10 @@ raw_path        = '/archive/20080_PD_EBRAINS/ORIGINAL/MEG';
 meg_path        = '/home/mikkel/PD_long/data_share/sourcedata';
 subj_data_path  = '/home/mikkel/PD_long/subj_data/';
 
+skip_subjs =  ['0582', '0604']
+
 #%% Overwrite
-overwrite=True
+overwrite=False
 
 #%% Read data
 alldata = pd.read_csv(op.join(subj_data_path, 'metadata.csv'))
@@ -26,20 +28,28 @@ for ii, ss in enumerate(linkdata['subjects']):
     subj = '0'+str(ss)
     sid  = str(linkdata['anonym_id'][ii]).zfill(3)
     print(subj)
+    
+    if subj in skip_subjs:
+        continue
 
     # I/O
     subdir_raw = op.join(raw_path, linkdata['subject_date'][ii] )
-    subdir_tmp = op.join(meg_path, 'sub-'+sid)
-    outFname = op.join(subdir_tmp)
-    
-    if not op.exists(subdir_tmp):
-        os.makedirs(subdir_tmp)
+    subdir_tmp = op.join(meg_path, linkdata['subject_date'][ii] )
         
     tmpFiles = [f for f in os.listdir(subdir_raw) if 'rest_ec_mc_avgtrans_tsss_corr95' in f]
-    outFname = op.join(subdir_tmp, tmpFiles[0])
+    inFile = tmpFiles[0]
+    outFname = op.join(subdir_tmp, inFile[:-4]+'-raw.fif')
+    
+    # Only run if overwrite = True
+    if op.exists(outFname) and not overwrite:
+        continue
+    
+    # Create output folder
+    if not op.exists(subdir_tmp):
+        os.makedirs(subdir_tmp)
 
     # Read MEG data
-    raw = mne.io.read_raw(op.join(subdir_raw, tmpFiles[0]))
+    raw = mne.io.read_raw(op.join(subdir_raw, inFile))
     
     # Anonymize
     mne.io.anonymize_info(raw.info)
@@ -52,7 +62,7 @@ for ii, ss in enumerate(linkdata['subjects']):
     raw.pick_types(meg=True, eog=True, ecg=True, ias=True, stim=True, syst=True, chpi=True, include=accChname)
     
     # Save
+    print('Saving '+outFname)
     raw.save(outFname, overwrite=overwrite)
-
 
 #END    
